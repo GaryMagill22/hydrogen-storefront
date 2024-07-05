56e49b7515b489668c316fc5eac88b269c29c9b3
Gary Magill <123419905+GaryMagill22@users.noreply.github.com>
2024-07-05 09:08:19 -0600

    storefront using tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,10 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js, jsx, ts, tsx}'],
   theme: {
+    container: {
+      center: true,
+      padding: '1rem'
+    },
     extend: {},
   },
   plugins: [require('@tailwindcss/typography')],
